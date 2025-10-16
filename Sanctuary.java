c460c5eea6143c6fa293eea1eb008cce4842b154
167tren <marcel.leon.terenyi@gmail.com>
2025-10-16 14:01:11 +0200

    Add relic catalog and world structure types

---
+++ Sanctuary.java
@@ -0,0 +1,17 @@
+public final class Sanctuary {
+    private final int tileX;
+    private final int tileY;
+
+    public Sanctuary(int tileX, int tileY) {
+        this.tileX = tileX;
+        this.tileY = tileY;
+    }
+
+    public int getTileX() {
+        return tileX;
+    }
+
+    public int getTileY() {
+        return tileY;
+    }
+}
